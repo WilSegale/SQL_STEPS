f891d141fb37f24707e9d7c19c5fb146f143d83e
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-01-11 18:59:19 -0500

    MYSQL

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,14 @@
 clear
-echo "Connecting to MySQL Database...."
-sleep 3
+echo "Connecting to MySQL Database"
+sleep 1
+clear
+echo "Connecting to MySQL Database."
+sleep 1
+clear
+echo "Connecting to MySQL Database.."
+sleep 1
+clear
+echo "Connecting to MySQL Database..."
 clear
 echo "Connected to MySQL"
 mysql -u root -p -A
