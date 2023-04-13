c116770cfc6a1dc392bad13419f4ccc8c4a7faf3
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-04-13 16:56:54 -0400

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,14 +1,19 @@
-clear
-echo "Connecting to MySQL Database"
-sleep 1
-clear
-echo "Connecting to MySQL Database."
-sleep 1
-clear
-echo "Connecting to MySQL Database.."
-sleep 1
-clear
-echo "Connecting to MySQL Database..."
-clear
-echo "Connected to MySQL"
-mysql -u root -p -A
+#!/bin/bash
+if command -v mysql &> /dev/null; then
+     echo "MySQL is installed"
+     clear
+     echo "Connecting to MySQL Database"
+     sleep 1
+     clear
+     echo "Connecting to MySQL Database."sleep 1
+     clear
+     echo "Connecting to MySQL Database.."
+     sleep 1
+     clear
+     echo "Connecting to MySQL Database..."
+     clear
+     echo "Connected to MySQL"
+     mysql -u root -p -A
+else
+     echo "MySQL is not installed"
+fi
