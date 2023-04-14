09e0780e616a0d2c4b7643c0a7f969d935307c96
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-04-14 12:46:41 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from sys import platform
 import os
+import subprocess
 
-if platform == "win32":
-    os.system("del *.sh")
-else:
-    os.system("rm -rf *.bat")
+try:
+    subprocess.check_output("mysql --version", shell=True)
+    print("MySQL is installed on this computer.")
+    if platform == "win32":
+        os.system("del *.sh")
+    else:
+        os.system("rm -rf *.bat")
+except subprocess.CalledProcessError:
+    print("MySQL is not installed on this computer.")
