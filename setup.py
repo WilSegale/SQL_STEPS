f4b148dc61266e0838d0bc85ee15f6d90063e806
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-04-14 23:38:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,15 @@
 from sys import platform
 import os
 import subprocess
-import signal
 
-def timeout_handler(signum, frame):
-    raise TimeoutError("Timeout expired.")
-
-def do_something():
-    # do something that may take a long time
-    try:
-        subprocess.check_output("mysql --version", shell=True)
-        print("MySQL is installed on this computer.")
-        if platform == "win32":
-            os.system("del *.sh")
-        else:
-            os.system("rm -rf *.bat")
-            
-    except subprocess.CalledProcessError:
-        print("MySQL is not installed on this computer.")
-
-# Set a timeout of 5 seconds
-signal.signal(signal.SIGABRT, timeout_handler)
-
+# do something that may take a long time
 try:
-    do_something()
-except TimeoutError:
-    print("Timed out")
-
+    subprocess.check_output("mysql --version", shell=True)
+    print("MySQL is installed on this computer.")
+    if platform == "win32":
+        os.system("del *.sh")
+    else:
+        os.system("rm -rf *.bat")
+        
+except subprocess.CalledProcessError:
+    print("MySQL is not installed on this computer.")
