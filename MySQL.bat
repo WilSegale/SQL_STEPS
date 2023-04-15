fe6ede4ad8aa4b2530d909ed62ddbe8edebfd12e
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-04-14 23:44:43 -0400

    Update MySQL.bat

--- MySQL.bat
+++ MySQL.bat
@@ -1,13 +1,13 @@
 @echo off
 
 :: Tells the user to in put there hostname like (localhost) etc
-set /p host="Enter MySQL host: "
+set /p host="Enter MySQL host example(localhost): "
 
 :: Tells the user to input there username
-set /p user="Enter MySQL user: "
+set /p user="Enter MySQL user example(root): "
 
 :: hides the users input for the password for MySQL
-set "psCommand=powershell -Command "$pword = read-host 'Enter password' -AsSecureString ; ^
+set "psCommand = powershell -Command "$pword = read-host 'Enter password' -AsSecureString ; ^
     [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword))""
 for /f "usebackq delims=" %%p in (`%psCommand%`) do set "password=%%p"
 
