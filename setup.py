6451af7246c90dc043003f3d5b89165031ce5950
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-21 14:07:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
-from sys import platform
-from colorama import *
 import os
 import subprocess
+from colorama import Fore
 
+# Define color constants
 GREEN = Fore.GREEN
+RED = Fore.RED
 
-# do something that may take a long time
+# Check if MySQL is installed on the computer
 try:
+    # Use subprocess to check the MySQL version
     subprocess.check_output("mysql --version", shell=True)
     print(f"{GREEN}MySQL is installed on this computer.")
-    if platform == "win32":
+
+    # Perform platform-specific actions
+    if os.name == "nt":  # Windows
+        # Delete shell scripts (*.sh files)
         os.system("del *.sh")
     else:
+        # Remove batch files (*.bat files)
         os.system("rm -rf *.bat")
         
 except subprocess.CalledProcessError:
-    print("MySQL is not installed on this computer.")
+    print(f"{RED}MySQL is not installed on this computer.")
